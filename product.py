0b94e78e0fdbca626335c35a436a66e721befe75
resteve <resteve@zikzakmedia.com>
2015-09-16 15:57:45 +0200

    Get Quantity with storage and input location + search quantity

--- product.py
+++ product.py
@@ -1,7 +1,7 @@
 #This file is part stock_user module for Tryton.
 #The COPYRIGHT file at the top level of this repository contains
 #the full copyright notices and license terms.
-from trytond.pool import PoolMeta
+from trytond.pool import Pool, PoolMeta
 from trytond.transaction import Transaction
 
 __metaclass__ = PoolMeta
@@ -13,11 +13,28 @@ class Product:
 
     @classmethod
     def get_quantity(cls, products, name):
+        Location = Pool().get('stock.location')
+
         context = Transaction().context
         # not locations + stock_warehouse in context
-        if 'locations' not in context and context.get('stock_warehouse'):
-            location_ids = [context['stock_warehouse']]
+        if not context.get('locations') and context.get('stock_warehouse'):
+            warehouse = Location(context['stock_warehouse'])
+            location_ids = [warehouse.storage_location.id, warehouse.input_location.id]
             with Transaction().set_context(locations=location_ids):
                 return cls._get_quantity(products, name, location_ids, products)
         # return super (with locations in context)
         return super(Product, cls).get_quantity(products, name)
+
+    @classmethod
+    def search_quantity(cls, name, domain=None):
+        Location = Pool().get('stock.location')
+
+        context = Transaction().context
+        # not locations in context
+        if not context.get('locations') and context.get('stock_warehouse'):
+            warehouse = Location(context['stock_warehouse'])
+            location_ids = [warehouse.storage_location.id, warehouse.input_location.id]
+            with Transaction().set_context(locations=location_ids):
+                return cls._search_quantity(name, location_ids, domain)
+        # return super (with locations in context)
+        return super(Product, cls).search_quantity(name, domain=None)
