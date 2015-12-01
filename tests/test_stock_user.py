679ebe1a2f27d1791b5ecf7465d055b338933416
resteve <resteve@zikzakmedia.com>
2015-12-01 01:07:57 +0100

    Tests

--- tests/test_stock_user.py
+++ tests/test_stock_user.py
@@ -1,27 +1,14 @@
-# This file is part of stock_user module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
-
-from trytond.tests.test_tryton import test_view, test_depends
-import os
-import sys
-import trytond.tests.test_tryton
+# This file is part of the stock_user module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 import unittest
+import trytond.tests.test_tryton
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class StockUserTestCase(unittest.TestCase):
+class StockUserTestCase(ModuleTestCase):
     'Test Stock User module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('stock_user')
-
-    def test0005views(self):
-        'Test views'
-        test_view('stock_user')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'stock_user'
 
 
 def suite():
@@ -29,6 +16,3 @@ def suite():
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
         StockUserTestCase))
     return suite
-
-if __name__ == '__main__':
-    unittest.TextTestRunner(verbosity=2).run(suite())
