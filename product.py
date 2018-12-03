2d3ae0b957943b769fa8ada5f84c015ab767a4f0
Raimon Esteve <raimon@nan-tic.com>
2018-12-03 21:54:26 +0100

    Get qty with childs and stock_date depends is product field or forecast #036128

--- product.py
+++ product.py
@@ -1,6 +1,7 @@
 #This file is part stock_user module for Tryton.
 #The COPYRIGHT file at the top level of this repository contains
 #the full copyright notices and license terms.
+import datetime
 from trytond.pool import Pool, PoolMeta
 from trytond.transaction import Transaction
 
@@ -13,15 +14,20 @@ class Product:
 
     @classmethod
     def get_quantity(cls, products, name):
-        Location = Pool().get('stock.location')
+        pool = Pool()
+        Location = pool.get('stock.location')
+        Date = pool.get('ir.date')
 
+        stock_date = Date.today() if name == 'product' else datetime.date.max
         context = Transaction().context
+
         # not locations + stock_warehouse in context
         if not context.get('locations') and context.get('stock_warehouse'):
             warehouse = Location(context['stock_warehouse'])
             location_ids = [warehouse.storage_location.id, warehouse.input_location.id]
             product_ids = map(int, products)
-            with Transaction().set_context(locations=location_ids):
+            with Transaction().set_context(locations=location_ids,
+                    stock_date_end=stock_date, with_childs=True):
                 return cls._get_quantity(products, name, location_ids, grouping_filter=(product_ids,))
         # return super (with locations in context)
         return super(Product, cls).get_quantity(products, name)
